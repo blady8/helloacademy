d66accea76b1b6b39c8c33c5d206563217ba1a55
Beatrice Testa (c) <beatrice.testa@avanade.com>
2019-10-10 10:06:54 +0200

    First Commit

---
+++ HelloAcademy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HelloAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FunzioniMatematiche.RecuperaDivisioneEDividendoEDividi();
+
+            //FunzioniRubrica.InserisciPersoneEMostraRubrica();
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - Divisione");
+            Console.WriteLine("2 - Rubrica semplice");
+            Console.WriteLine("3 - Rubrica complessa");
+            Console.WriteLine("0 - Exit");
+            Console.Write("Selezione: ");
+            var selezione = ConsoleUtils.LeggiNumeroInteroDaConsole(1,3);
+
+            //Selezione della funzione da avviare
+            switch(selezione)
+            {
+                case 1:
+                    FunzioniMatematiche.RecuperaDivisioneEDividendoEDividi();
+                    break;
+                case 2:
+                    FunzioniMatematiche.RecuperaDivisioneEDividendoEDividi();
+                    break;
+                case 3:
+                    FunzioniMatematiche.RecuperaDivisioneEDividendoEDividi();
+                    break;
+                case 0:
+                    FunzioniMatematiche.RecuperaDivisioneEDividendoEDividi();
+                    break;
+
+
+
+
+            }
+
+            FunzioniRubrica.InserisciNumeroArbitrarioPersoneInRubrica();
+
+
+
+
+
+
+
+        }
+    }    
+}
